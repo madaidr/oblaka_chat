f9a691331fecb9c247c10f4fc5884a7f0bbf331e
madaidr <yxzc@yahoo.com>
2021-01-12 13:55:15 +0300

    new

---
+++ oblaka_chat/Startup.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Threading.Tasks;
+
+[assembly: OwinStartup(typeof(oblaka_chat.Startup))]
+
+namespace oblaka_chat
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Any connection or hub wire up and configuration should go here
+            app.MapSignalR();
+        }
+    }
+}
